1c4a96f88191fade00c2dd69d79949568b619cde
Emillyme <emill.ymello212@gmail.com>
2024-10-16 17:02:31 -0300

    teste

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <main>
+      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
+        <BoxProject/>
         <BoxProject/>
       </main>
     </>
--- src/app/components/BoxProject/boxproject.tsx
+++ src/app/components/BoxProject/boxproject.tsx
@@ -4,22 +4,17 @@ import Link from "next/link";
 
 export default function BoxProject() {
   return (
-    <>
-      <main className="p-8">
-        <div className="grid grid-cols-5 gap-3">
-          {/* Caixa do projeto com tamanho personalizado */}
-          <div className="bg-white rounded-lg shadow-xl overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl duration-300 ease-out cursor-pointer w-[290px] h-[200px]">
-            {/* Parte superior roxa */}
-            <div className="bg-purple-400 h-[90px]"></div>
-            {/* Conteúdo do projeto */}
-            <div className="p-4">
-              <h3 className="text-xl font-medium">Projeto 1</h3>
-              <p className="text-slate-600 mb-3">GS/OIS-LA</p>
-              <p className="text-slate-400">01/10/23 | 07 AM - 08 PM</p>
-            </div>
-          </div>
-        </div>
-      </main>
-    </>
+    <div className="bg-white rounded-lg 
+    shadow-xl overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl 
+    duration-300 ease-out cursor-pointer w-[290px] h-[200px]">
+      {/* Parte superior roxa */}
+      <div className="bg-purple-400 h-[90px]"></div>
+      {/* Conteúdo do projeto */}
+      <div className="p-4">
+        <h3 className="text-xl font-medium">Projeto 1</h3>
+        <p className="text-slate-600 mb-2">GS/OIS-LA</p>
+        <p className="text-slate-400">01/10/23 | 07 AM - 08 PM</p>
+      </div>
+    </div>
   );
 }
