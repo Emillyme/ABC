a71d2c2916909dcafff23e922a1e1f26f7c323f7
Emillyme <emill.ymello212@gmail.com>
2024-10-22 13:35:34 -0300

    Props

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -5,16 +5,12 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
-        <BoxProject/>
+      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-10">
+        <BoxProject title="" area="" details=""/>
+        <BoxProject title="" area="" details=""/>
+        <BoxProject title="" area="" details=""/>
+        <BoxProject title="" area="" details=""/>
+        <BoxProject title="" area="" details=""/>
       </main>
     </>
   );
--- src/app/components/BoxProject/boxproject.tsx
+++ src/app/components/BoxProject/boxproject.tsx
@@ -2,19 +2,27 @@ import Image from "next/image";
 import logo from "/public/Logo.svg";
 import Link from "next/link";
 
-export default function BoxProject() {
+type Props = {
+  title: string
+  area: string
+  details: string
+}
+
+export default function BoxProject({title, area, details} : Props) {
   return (
+    <div className="p-[10px]">
     <div className="bg-white rounded-lg 
-    shadow-xl overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl 
+    shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl
     duration-300 ease-out cursor-pointer w-[290px] h-[200px]">
       {/* Parte superior roxa */}
       <div className="bg-purple-400 h-[90px]"></div>
       {/* Conteúdo do projeto */}
       <div className="p-4">
-        <h3 className="text-xl font-medium">Projeto 1</h3>
-        <p className="text-slate-600 mb-2">GS/OIS-LA</p>
-        <p className="text-slate-400">01/10/23 | 07 AM - 08 PM</p>
+        <h3 className="text-xl font-medium">{title}</h3>
+        <p className="text-slate-600 mb-2">{area}</p>
+        <p className="text-slate-400">{details}</p>
       </div>
+    </div>
     </div>
   );
 }
